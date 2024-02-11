3d813037258886bd2cb764d7aa2f772652bbfd0e
Ruslan <arhara81@gmail.com>
2024-02-11 17:42:48 +0300

    hw_2 refactoring

--- unit_test_hw/hw_2/hw_2/Test/seminars/second/hw/VehicleTest.java
+++ unit_test_hw/hw_2/hw_2/Test/seminars/second/hw/VehicleTest.java
@@ -9,17 +9,20 @@ class VehicleTest {
     @Test
     public void testCarIsInstanceOfVehicle() {
         Car car = new Car("Dodge", "Ram", 2010);
+
         assertTrue(car instanceof Vehicle);
     }
     @Test
     public void testCarHaveFourWheels() {
         Car car = new Car("Lada", "Granta", 2020);
+
         assertEquals(4, car.getNumWheels());
     }
 
     @Test
     public void testMotorcycleHaveTwoWheels() {
         Motorcycle motorcycle = new Motorcycle("Yamaha", "R", 2022);
+
         assertEquals(2, motorcycle.getNumWheels());
     }
 
@@ -34,6 +37,8 @@ class VehicleTest {
     @Test
     public void testMotorcycleSpeedInTestDrive() {
         Motorcycle motorcycle = new Motorcycle("Kawasaki", "Ninja", 2021);
+        motorcycle.testDrive();
+
         assertThat(motorcycle.getSpeed()).isEqualTo(75);
     }
 
@@ -42,6 +47,7 @@ class VehicleTest {
         Car car = new Car("Honda", "Accord", 2000);
         car.testDrive();
         car.park();
+
         assertThat(car.getSpeed()).isEqualTo(0);
     }
 
@@ -50,6 +56,7 @@ class VehicleTest {
         Motorcycle motorcycle = new Motorcycle("Java", "350", 2024);
         motorcycle.testDrive();
         motorcycle.park();
+
         assertThat(motorcycle.getSpeed()).isEqualTo(0);
     }
 }
